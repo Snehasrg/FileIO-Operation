b1b851168ecfb89418e3b9b774dd4bd63a73fb66
Snehasrg <sghongade642@gmail.com>
2023-06-29 21:35:37 +0530

    [sneha]Added Program for File IO operation

---
+++ AddressBook.cs
@@ -0,0 +1,13 @@
+﻿namespace FileIO
+{
+    public class AddressBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
